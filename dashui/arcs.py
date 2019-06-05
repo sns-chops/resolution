c3eddab3041e0193139e11ebb24a9f62ddcb3982
Jiao Lin <jiao.lin@gmail.com>
2019-06-05 00:34:29 +0000

    fixed paths

--- dashui/arcs.py
+++ dashui/arcs.py
@@ -1,3 +1,6 @@
+# to run, add parent directory of PyChop to PYTHONPATH
+# then $ python {thisfile}
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
--- dashui/arcsmodel.py
+++ dashui/arcsmodel.py
@@ -1,12 +1,13 @@
 
 import os, sys
-sys.path.insert(0, os.path.expanduser('~/dv'))
+# sys.path.insert(0, os.path.expanduser('~/dv'))
+here = os.path.abspath(os.path.dirname(__file__))
 
 import numpy as np
 
 from PyChop import PyChop2
 
-yamlpath = os.path.expanduser('~/dv/resolution/ARCS/PyChop/arcs-opt.yaml')
+yamlpath = os.path.join(here, '../ARCS/PyChop/arcs-opt.yaml')
 
 
 def res_vs_E(E, chopper='ARCS-100-1.5-SMI', chopper_freq=600., Ei=100.):
