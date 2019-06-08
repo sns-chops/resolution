975dfb6827f36f9ffb1756159b89e40c361db6d3
Jiao Lin <linjiao@ornl.gov>
2019-06-08 07:21:19 -0400

    new file:   arcs/exp.py. forgot to check in

---
+++ dashui/arcs/exp.py
@@ -0,0 +1,14 @@
+import os, sys
+here = os.path.abspath(os.path.dirname(__file__))
+
+# data
+import resolution_plot
+fc1data = resolution_plot.ExpData(os.path.join(here, '../../ARCS//V_Cali_Int_Res_FC1_2018_v2.dat'))
+fc2data = resolution_plot.ExpData(os.path.join(here, '../../ARCS/V_Cali_Int_Res_FC2_2018_v2.dat'))
+fc1_highres_data = resolution_plot.ExpData(os.path.join(here, '../../ARCS/V_Cali_Int_Res_FC1_HighRes_2018_v2.dat'))
+vscatt_scale = 2.6e4
+vscatt_scale *=60./1.4
+vscatt_scale *= 1.5/2
+fc1data.intensity *= vscatt_scale
+fc2data.intensity *= vscatt_scale
+fc1_highres_data.intensity *= vscatt_scale
