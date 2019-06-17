2f4c18eeaaed586217321162a327195f96933d99
Jiao Lin <linjiao@ornl.gov>
2019-06-17 16:53:32 -0400

    Refs #7. implemented

--- dashui/cncs/model.py
+++ dashui/cncs/model.py
@@ -1,3 +1,10 @@
+"""
+We are applying corrections to PyChop model to match the experimental data.
+However, we should actually apply the correction to the experimental data to match 
+model.
+"""
+
+
 import os, sys
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,7 +14,7 @@ from PyChop import PyChop2
 
 yamlpath = os.path.join(here, '../../CNCS/PyChop/cncs-06082019.yaml')
 
-scale_flux = 600
+scale_flux = 7e-6
 
 freqs = {
     'High Resolution': 180.,
@@ -25,13 +32,32 @@ def elastic_res_flux(chopper, Ei=100.):
     instrument = PyChop2(yamlpath)
     instrument.setChopper(chopper)
     res, flux = instrument.getResFlux(Etrans=0., Ei_in=Ei, frequency=freqs[chopper])
-    return res[0], flux[0]*scale_flux
+    return res[0], flux[0]*scale_flux*deteff(Ei)*absorption(Ei)
 
 def main():
     E = np.arange(0., 100., 5.)
     res = res_vs_E(E, 'Intermediate')
     print(res)
     return
+
+# corrections for flux
+import unit_conversion as Conv
+# He3
+v =  8.314*(300.)/(10*101325)/6.022e23 * 1e30  # AA^3
+xs = 5333. # barn
+mu_He3 = xs/v
+pixelsize = 2.54 * .7
+def deteff(E):
+    v = Conv.e2v(E)
+    return 1.-np.exp(-2200./v*mu_He3*pixelsize)
+# V
+a = 3.03 # AA
+xs = 5 # barn
+mu_V = xs/a**3  # 1./cm
+V_thickness = .9 #cm
+def absorption(E):
+    v = Conv.e2v(E)
+    return np.exp(-2200./v*V_thickness*mu_V)
 
 if __name__ == '__main__': main()
 
