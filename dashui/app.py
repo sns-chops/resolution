c0e60f4a9cfeb83e6af2f001a0bc177ce2180422
Jiao Lin <linjiao@ornl.gov>
2019-07-06 15:09:14 -0400

    webapp: added statcounter

--- dashui/app.py
+++ dashui/app.py
@@ -31,8 +31,10 @@ for instr in instruments:
     tabs.append(tab)
 
 app.layout = html.Div([
-    dcc.Tabs(tabs, vertical=True)
+    dcc.Tabs(tabs, vertical=True),
 ])
+app.scripts.append_script({"external_url": "/assets/statcounter.js"})
+
 
 for instr in instruments:
     mod = eval(instr)
---
+++ dashui/assets/statcounter.js
@@ -0,0 +1,7 @@
+var sc_project=12039638;
+var sc_invisible=0;
+var sc_security="9b21ec11";
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" +
+	       scJsHost+
+	       "statcounter.com/counter/counter.js'></"+"script>");
